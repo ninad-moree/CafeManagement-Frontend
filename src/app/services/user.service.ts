78b77fa357eae380d550ad496ae7598fc35f86a1
Ninad More <ninadmore183@gmail.com>
2024-09-24 16:10:57 +0530

    Dashboard Page Completed and API integrated

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -39,4 +39,8 @@ export class UserService {
       }
     );
   }
+
+  checkToken() {
+    return this.httpClient.get(this.url + "/user/checkToken");
+  }
 }
--- src/app/shared/global-constants.ts
+++ src/app/shared/global-constants.ts
@@ -1,9 +1,11 @@
 export class GlobalConstants {
-    public static genericError: string = "Something went wron. Please try again later!!";
+    public static genericError: string = "Something went wrong. Please try again later!!";
 
     public static nameRegex: string = "[a-zA-Z0-9 ]*";
     public static emailRegex: string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
     public static contactRegex: string = "^[e0-9]{10,10}$";
 
-    public static error:string = "error"
+    public static error:string = "error";
+
+    public static unautorised:string = "You are not authorised person to access this page.";
 }
