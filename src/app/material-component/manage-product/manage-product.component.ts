d0e896be6693c036dcea4245c0b2f730964ee117
Ninad More <ninadmore183@gmail.com>
2024-09-26 14:56:47 +0530

    Manage Product UI created

---
+++ src/app/material-component/manage-product/manage-product.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { ProductService } from 'src/app/services/product.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { GlobalConstants } from 'src/app/shared/global-constants';
+
+@Component({
+  selector: 'app-manage-product',
+  templateUrl: './manage-product.component.html',
+  styleUrls: ['./manage-product.component.scss']
+})
+export class ManageProductComponent implements OnInit {
+  displayedColumns:string[] = ['name', 'categoryName', 'description', 'price', 'edit'];
+  dataSource:any;
+  // length:any;
+  responseMessage:any;
+
+  constructor(
+    private productService: ProductService,
+    private ngxService:NgxUiLoaderService,
+    private dialog:MatDialog,
+    private snackBarService:SnackbarService,
+    private router:Router
+  ) {}
+
+  ngOnInit(): void {
+    this.ngxService.start();
+    this.tableData();
+  }
+
+  tableData() {
+    this.productService.getProducts().subscribe((response:any) => {
+      this.ngxService.stop();
+      this.dataSource = new MatTableDataSource(response);
+    }, (error) => {
+      this.ngxService.stop();
+      if(error.error?.message) {
+        this.responseMessage = error.error?.message;
+      } else {
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackBarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    });
+  }
+
+  applyFilter(event:Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  handleAddAction() {
+
+  }
+
+  handleEditAction(values:any) {
+
+  }
+
+  handleDeleteAction(values:any) {
+
+  }
+
+  onChange(status:any, id:any) {}
+
+}
