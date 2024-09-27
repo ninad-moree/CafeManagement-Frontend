308dc9f7f284e515eaece70cfa025039b2b891b5
Ninad More <ninadmore183@gmail.com>
2024-09-27 12:06:32 +0530

    View Bill, Delete Bill and Download Bill implemented

--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -24,4 +24,10 @@ export class BillService {
   getBills() {
     return this.htpClient.get(this.url + "/bill/getBills");
   }
+
+  delete(id:any) {
+    return this.htpClient.post(this.url + "/bill/delete/" + id, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    });
+  }
 }
